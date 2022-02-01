5d7ee95c9751218a755deea5c56f93ac90b7fe45
Sayantan Mondal <62999423+MondalSayantan@users.noreply.github.com>
2022-02-01 12:08:06 +0530

    Finished Gatsby Tutorial Part 3

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/layout";
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About">
+      <p>Excited to learn Gatsby</p>
+    </Layout>
+  );
+};
+
+export default AboutPage;
